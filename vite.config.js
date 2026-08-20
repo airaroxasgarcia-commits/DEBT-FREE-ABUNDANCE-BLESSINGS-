ccb1a32d453fc6f21fdeb87fefa8e964e8da36de
airaroxasgarcia-commits <airaroxasgarcia@gmail.com>
2026-08-20 15:12:40 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Deployed at https://airaroxasgarcia-commits.github.io/DEBT-FREE-ABUNDANCE-BLESSINGS-/
+export default defineConfig({
+  plugins: [react()],
+  base: "/DEBT-FREE-ABUNDANCE-BLESSINGS-/",
+});
